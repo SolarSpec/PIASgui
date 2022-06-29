cdb32025dcec50e2e8de7d29d4f36eb4668452e6
dudeImpossible42 <hvidimlic1234@hotmail.com>
2022-06-28 20:40:27 -0400

    Updated PIASloadTDMS for efficiency

--- MATLAB data scripts/PIASloadTDMSfolder.m
+++ MATLAB data scripts/PIASloadTDMSfolder.m
@@ -25,7 +25,7 @@ for FileIndex = 1:1:size(FolderContent,1)
         TDMScontent = TDMS_readTDMSFile([FolderPath, '\', FolderContent(FileIndex).name]);
 
         % Find all useful data in CH0 - only look at A-B trace or just A trace
-        CH0GroupIndex = find(strcmp(TDMScontent.groupNames,'CH0')); % CH0 index is always 2??
+        CH0GroupIndex = strcmp(TDMScontent.groupNames,'CH0'); % CH0 index is always 2??
         ChannelIndices = TDMScontent.chanNames{1,CH0GroupIndex};
 
         if count == 1
@@ -50,17 +50,17 @@ for FileIndex = 1:1:size(FolderContent,1)
 
         % Maybe instead do check if contains first, might be faster then find
         % index always. Just do inside the if statemnt if possible??
-        AbsChanIndex = find(strcmp(ChannelIndices,name));
+        AbsChanIndex = strcmp(ChannelIndices,name);
 
         % This block only runs after first file, what if others don't have the selected name?
         %%ALSO NEED TO CHECK IF THE FIRST AND LAST FIVE ELEMENTS ARE ZERO THAT
         %%WE KNOW ITS AN EMPTY ARRAY AND THEN CONTINUE TO NEXT HEIERARCHY
 
-        if isempty(AbsChanIndex)
+        if ~any(AbsChanIndex)
             given = contains(ChannelIndices,selectedChans);
             NewOpts = ChannelIndices(given);
             
-            AbsChanIndex = find(strcmp(ChannelIndices,NewOpts{1,1}));
+            AbsChanIndex = strcmp(ChannelIndices,NewOpts{1,1});
             app.Channels = [app.Channels;string(NewOpts{1,1})];
             app.RedGreen = [app.RedGreen;0];
         else
@@ -70,15 +70,15 @@ for FileIndex = 1:1:size(FolderContent,1)
 
         AbsDataIndex = TDMScontent.chanIndices{1,CH0GroupIndex}(AbsChanIndex);
 
-        if isempty(find(TDMScontent.data{1,AbsDataIndex}(1:5), 1)) == 1 && isempty(find(TDMScontent.data{1,AbsDataIndex}(end-4:end), 1)) == 1 
+        if ~any(TDMScontent.data{1,AbsDataIndex}(1:5)) == 1 && ~any(TDMScontent.data{1,AbsDataIndex}(end-4:end)) == 1 
             continue % DO NOT ADD IF COLUMN IS FULL OF ZEROS
         end
 
         Abs = transpose(TDMScontent.data{1,AbsDataIndex});
 
         % Extract 
-        BkgGroupIndex = find(strcmp(TDMScontent.groupNames,'Background level'));
-        BkgChanIndex = find(strcmp(TDMScontent.chanNames{1,BkgGroupIndex},'Avg bkg level (V)'));
+        BkgGroupIndex = strcmp(TDMScontent.groupNames,'Background level');
+        BkgChanIndex = strcmp(TDMScontent.chanNames{1,BkgGroupIndex},'Avg bkg level (V)');
         BkgDataIndex = TDMScontent.chanIndices{1,BkgGroupIndex}(BkgChanIndex);
 
         Bkg = TDMScontent.data{1,BkgDataIndex};
@@ -88,7 +88,7 @@ for FileIndex = 1:1:size(FolderContent,1)
     end
 end
 
-TimeGroupIndex = find(strcmp(TDMScontent.groupNames,'Time'));
+TimeGroupIndex = strcmp(TDMScontent.groupNames,'Time');
 TimeDataIndex = TDMScontent.chanIndices{1,TimeGroupIndex};
 
 %Check the first 10 index differences to get a delta double array
